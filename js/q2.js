f4ceea0f751e7cbf0fe7a187aa3cc819f7019752
ibrahim18101996 <brhomd.96@gmail.com>
2020-03-16 13:10:32 +0200

    q2 done

--- js/q2.js
+++ js/q2.js
@@ -1,1 +1,12 @@
 'use strict'
+function printOut(){
+    for (var i =0 ;i<5;i++){
+        var star ='';
+        for (var n=0 ;n<=i ;n++){
+        star+='*';
+    }
+    console.log(star);
+}
+    
+}
+printOut();
