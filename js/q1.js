d3598f0772b56f6ac89fa568a08d31e8b049a6f9
ibrahim18101996 <brhomd.96@gmail.com>
2020-03-16 13:02:24 +0200

    q1 done

--- js/q1.js
+++ js/q1.js
@@ -1,1 +1,12 @@
 'use strict'
+function greaterThan(arr,a){
+    var cont=0;
+for (var i=0 ;i<arr.length;i++){
+if (arr[i]>a){
+cont++;
+}
+}
+console.log(cont);
+return cont;
+}
+greaterThan([5,7,6,3,2],3);
